1aad0eef6c8a62947363df550847683d929ac175
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-01-31 13:12:21 -0800

    initial commit and PR for Tabs Components LAMBDA January 31,2019

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,25 +1,40 @@
+
+	// 	<div class="header-container">
+  //   <div class="dropdown">
+  //     <div class="dropdown-button">Dropdown</div>
+  //     <div class="dropdown-content dropdown-hidden">
+  //       <a class="dropdown-link" href="https://lambdaschool.com/" target="_blank">Lambda</a>
+  //       <a class="dropdown-link" href="https://www.google.com/" target="_blank">Google</a>
+  //       <a class="dropdown-link" href="https://developer.mozilla.org/en-US/" target="_blank">MDN</a>
+  //     </div>
+  //   </div>
+  //   <img src="./lambda_logo_light_small.png" class="header" />
+  // </div>
+
+
+
+
 class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.element;
+    this.element = element;
+
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector();
+    this.button = this.element.querySelector('.dropdown-button');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
+    this.content = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-
-    })
+    this.button.addEventListener('click', () => this.toggleContent())
   }
 
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
